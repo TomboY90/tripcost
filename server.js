9115f37f45397c33e80612364aee46fcd5771eb7
gmlee <gmlee@salin.co.kr>
2020-12-01 11:12:06 +0900

    Feat create server and make an endpoint for trips

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mongo = require('mongodb').MongoClient;
+const app = express();
+
+const URL = 'mongodb://localhost:27017'
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// connect mongoDB
+
+let db, trips, expenses
+
+mongo.connect(
+  URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    db = client.db('tripcost')
+    trips = db.collection('trips')
+    expenses = db.collection('expenses')
+  }
+)
+
+app.post('/trip', (req, res) => {
+  const name = req.body.name
+
+  trips.insertOne({ name: name }, (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+
+    res.status(200).json({ ok: true })
+  })
+})
+
+app.get('/trips', (req, res) => {
+  trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+
+    res.status(200).json({ trips: items })
+  })
+})
+
+app.post('/expense', (req, res) => {
+  /* */
+})
+app.get('/expenses', (req, res) => {
+  /* */
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
